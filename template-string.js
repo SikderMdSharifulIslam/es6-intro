d8cfc8bdddbedd8445a092cec2bed87c6a659d47
SikderMdSharifulIslam <sikdermdsharifulislam@gmail.com>
2026-02-26 14:30:45 +0600

    template string in js

---
+++ template-string.js
@@ -0,0 +1,8 @@
+const firstName = 'Sikder';
+const lastName = 'Shariful';
+
+const aboutMe = `My name is ${firstName}`;
+console.log(aboutMe);
+
+const giveMe = `give me money ${(10+20)*2+500}`;
+console.log(giveMe);
